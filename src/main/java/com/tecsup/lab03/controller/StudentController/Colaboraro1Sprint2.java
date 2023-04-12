95554582a2018b7622db7c9b09fd4c1181577b28
jals1 <jose.lazo@tecsup.edu.pe>
2023-04-11 23:51:57 -0500

    Responsable S2 (Lazo)

--- src/main/java/com/tecsup/lab03/controller/StudentController/Colaboraro1Sprint2.java
+++ src/main/java/com/tecsup/lab03/controller/StudentController/Colaboraro1Sprint2.java
@@ -1,10 +1,14 @@
 package com.tecsup.lab03.controller.StudentController;
 
 public class Colaboraro1Sprint2 {
-	
+	///Sinche
 	public String nombre;
 	public String apellidos;
 	public int cantidad;
+	///Lazo
+	public int numero_telefono;
+	public String direccion;
+	public int edad;
 	
 	
 }
